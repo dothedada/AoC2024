8976992a6ba5ce2ed75e17914a036db6bf29c21f
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-04 16:05:58 -0500

    ordena el código

--- day4/day4.mjs
+++ day4/day4.mjs
@@ -2,57 +2,30 @@ import { readFileSync } from "fs";
 
 const path = "./input.txt";
 const data = readFileSync(path, "utf-8");
-
-const searchFor = ["X", "M", "A", "S"];
-const dataLines = data.split("\n").map((line) => {
-  // return `${line}`;
-  return `---${line}---`;
-});
+const dataLines = data.split("\n").map((line) => `${line}_`);
 const lineLength = dataLines[0].length;
 const dataStream = dataLines.join("").split("");
+const searchFor = ["X", "M", "A", "S"];
 
-const up = (current) => current - lineLength;
-const upRight = (current) => current - lineLength + 1;
-const right = (current) => current + 1;
-const downRight = (current) => current + lineLength + 1;
-const down = (current) => current + lineLength;
-const downLeft = (current) => current + lineLength - 1;
-const left = (current) => current - 1;
-const upLeft = (current) => current - lineLength - 1;
-
-const checkNeighbors = (current, stream) => {
-  const neighbors = [
-    stream[upLeft(current)],
-    stream[upRight(current)],
-    stream[downRight(current)],
-    stream[downLeft(current)],
-  ];
-
-  if (
-    neighbors.filter((char) => char === "M").length === 2 &&
-    neighbors.filter((char) => char === "S").length === 2
-  ) {
-    const firstM = neighbors.indexOf("M");
-    const lastM = neighbors.lastIndexOf("M");
-    const diffM = Math.abs(firstM - lastM);
-
-    console.log(neighbors, diffM, stream[current]);
-    return diffM === 2 ? 0 : 1;
-  }
-  return 0;
+const paths = {
+  up: (current) => current - lineLength,
+  upRight: (current) => current - lineLength + 1,
+  right: (current) => current + 1,
+  downRight: (current) => current + lineLength + 1,
+  down: (current) => current + lineLength,
+  downLeft: (current) => current + lineLength - 1,
+  left: (current) => current - 1,
+  upLeft: (current) => current - lineLength - 1,
 };
 
-const paths = [up, upRight, right, downRight, down, downLeft, left, upLeft];
-
-let timesFound = 0;
-
+let timesXMASfound = 0;
 const searchPath = (path, baseIndex, stringIndex) => {
   const nextCharToLook = searchFor[stringIndex + 1];
   const nextCell = path(baseIndex);
   const nextCharInPath = dataStream[nextCell];
 
   if (!nextCharToLook) {
-    timesFound++;
+    timesXMASfound++;
     return;
   }
 
@@ -63,20 +36,37 @@ const searchPath = (path, baseIndex, stringIndex) => {
   return;
 };
 
+let timesX_MASfound = 0;
+const checkNeighbors = (current, stream) => {
+  const neighbors = [
+    stream[paths.upLeft(current)],
+    stream[paths.upRight(current)],
+    stream[paths.downRight(current)],
+    stream[paths.downLeft(current)],
+  ];
+
+  if (
+    neighbors.filter((char) => char === "M").length === 2 &&
+    neighbors.filter((char) => char === "S").length === 2
+  ) {
+    const firstM = neighbors.indexOf("M");
+    const lastM = neighbors.lastIndexOf("M");
+    const diffM = Math.abs(firstM - lastM);
+
+    return diffM === 2 ? 0 : 1;
+  }
+  return 0;
+};
+
 dataStream.forEach((char, index) => {
   if (char === searchFor[0]) {
-    for (const path of paths) {
-      searchPath(path, index, 0);
+    for (const direction in paths) {
+      searchPath(paths[direction], index, 0);
     }
   }
-});
-
-let timesXfound = 0;
-
-dataStream.forEach((char, index) => {
   if (char === "A") {
-    timesXfound += checkNeighbors(index, dataStream);
+    timesX_MASfound += checkNeighbors(index, dataStream);
   }
 });
 
-console.log(timesFound, timesXfound);
+console.log("xmas:", timesXMASfound, "x-mas:", timesX_MASfound);
